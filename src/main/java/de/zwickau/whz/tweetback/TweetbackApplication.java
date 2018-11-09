918a2ae012ef6274e64c8c6b9381516a690e42ea
murat <muratruslanov@gmail.com>
2018-11-09 12:25:11 +0100

    init message

---
+++ src/main/java/de/zwickau/whz/tweetback/TweetbackApplication.java
@@ -0,0 +1,12 @@
+package de.zwickau.whz.tweetback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweetbackApplication.class, args);
+    }
+}
---
+++ src/test/java/de/zwickau/whz/tweetback/TweetbackApplicationTests.java
@@ -0,0 +1,16 @@
+package de.zwickau.whz.tweetback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TweetbackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
